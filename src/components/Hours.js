27ae644c44133b6ba7a1ee3951ad9cf6899e307e
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-14 10:21:09 -0500

    Add some styling to hours comp

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -4,26 +4,28 @@ function Hours({location}) {
 
   return (
     <>
-    <div>
         <p><strong>Hours</strong></p>
-        <p>Mon:</p>
-        <p>Tue:</p>
-        <p>Wed:</p>
-        <p>Thu:</p>
-        <p>Fri:</p>
-        <p>Sat:</p>
-        <p>Sun:</p>
-    </div>
-    <div>
-        <p>{location.mon}</p>
-        <p>{location.tue}</p>
-        <p>{location.wed}</p>
-        <p>{location.thu}</p>
-        <p>{location.fri}</p>
-        <p>{location.sat}</p>
-        <p>{location.sun}</p>
-    </div>
-    </>
+        <div className="flex gap-[16px]">
+            <div>
+                <p>Mon:</p>
+                <p>Tue:</p>
+                <p>Wed:</p>
+                <p>Thu:</p>
+                <p>Fri:</p>
+                <p>Sat:</p>
+                <p>Sun:</p>
+            </div>
+            <div>
+                <p>{location.mon}</p>
+                <p>{location.tue}</p>
+                <p>{location.wed}</p>
+                <p>{location.thu}</p>
+                <p>{location.fri}</p>
+                <p>{location.sat}</p>
+                <p>{location.sun}</p>
+            </div>
+        </div>
+        </>
   )
 }
 
