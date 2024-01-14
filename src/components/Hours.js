b022625157a25477a806a4f5e064fcd84e891c7a
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-14 10:14:22 -0500

    Remove unnecessary code fom hours comp, since fetch no longer req'd

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,31 +1,6 @@
-import React, {useState, useEffect} from 'react'
-const QUERY = encodeURIComponent('*[_type == "hours"]')
-const URL = `https://${process.env.REACT_APP_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${process.env.REACT_APP_DATASET}?query=${QUERY}`;
-
-
-
-console.log(URL)
+import React from 'react'
 
 function Hours({location}) {
-    let [hours, setHours] = useState(null)
-
-    useEffect(() => {
-        getHours()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    // This function will fetch the hours that the store is open, and set it to the state variable hours
-    function getHours() {
-        fetch(URL)
-            .then((res) => res.json())
-            .then(({ result }) => {
-                if(result.length > 0) {
-                    setHours(result)
-                }
-                console.log(hours)
-            })
-            .catch((err) => console.error(err))
-    } 
 
   return (
     <>
