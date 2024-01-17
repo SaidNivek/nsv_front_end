f25ad1db325db23c39b58df9efdcb49584853cc7
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-16 20:41:16 -0500

    Add google map link to card and style link

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 function Address({location}) {
+  let google_query=`http://maps.google.com/?q=${location.street},${location.city},${location.state},${location.zip}`
+
   return (
     <div className="w-[299px] md:mt-[12px] mt-[24px]">
       <p className="address_card_medium_font">Address</p>
       <div className="address_card_small_font">
         <p>{location.street}</p>
-        <p>{location.city},{location.state} {location.zip}</p>
+        <p>{location.city}, {location.state} {location.zip}</p>
         <p>{location.phone}</p>
+        <a className="address_link" href={`${google_query}`} target="_blank" rel="noreferrer"><p className="mt-[12px]">Get Directions</p></a>
       </div>
     </div>
   )
