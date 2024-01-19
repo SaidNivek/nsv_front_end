e5803918f9ed793fd430ed711150899ad95370a4
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-18 23:33:14 -0500

    Brands comp added

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import Values from '../components/Values'
+import Brands from '../components/Brands'
 import AddressHoursCard from '../cards/AddressHoursCard'
 
 function Home() {
   return (
     <>
         <Values />
+        <Brands />
         <AddressHoursCard />
     </>
   )
