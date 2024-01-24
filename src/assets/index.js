027b83e09e3a67d7cd8d2e9fab05370a53c17a06
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-23 21:03:47 -0500

    Playing with bullseye image on valuecard

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,10 +2,12 @@ import close from './close.svg'
 import logo from './logo.svg'
 import menu from './menu.svg'
 import arrow from './arrow.svg'
+import bullseye from './bullseye.png'
 
 export {
     close,
     logo, 
     menu,
-    arrow
+    arrow,
+    bullseye
 }
--- src/components/cards/ValueCard.js
+++ src/components/cards/ValueCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { arrow } from '../../assets'
+import { arrow, bullseye } from '../../assets'
 
 function ValueCard({value}) {
+  // style={`background-image: url(${bullseye})`}
   return (
-    <div className="ValueCard flex w-[30%] sm:m-[1%] ">
-      <img src={arrow} alt="bullseye" className="md:w-[120px] md:h-[120px] w-[80px] h-[50px] md:absolute top-[46px] left-[152px] " />
+    <div className={`ValueCard flex w-[30%] sm:m-[1%] relative`} >
+      <img src={arrow} alt="bullseye" className="bg-inherit-50 bg-blend-multiply bg-cover bg-no-repeat md:w-[40%] md:h-[60%] w-[80px] h-[50px] md:absolute bottom-[75%] left-[30%] " />
       <h2 className="card_large_font ">{value.title}</h2>
       <p>{value.copy}</p>
     </div>
