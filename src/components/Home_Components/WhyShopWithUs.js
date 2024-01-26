3d6ac84ab692264a17fa90d269b6c036f97ea4d8
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-26 17:31:07 -0500

    Add SectionHead to WhyShopWithUs

--- src/components/Home_Components/WhyShopWithUs.js
+++ src/components/Home_Components/WhyShopWithUs.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { SectionHead } from '../../assets'
 
 function WhyShopWithUs() {
   return (
-    <div>WhyShopWithUs</div>
+    <div className="flex flex-col">
+    <div className="text-center relative">
+        <SectionHead text="Why shop with us?" width="296" height="30" viewBox="0 0 296 30" rect_x="0.293579" rect_y="0.299316" rect_width="295.413" rect_height="28.8819" />
+    </div>    
+  </div>
   )
 }
 
 export default WhyShopWithUs
+
+{/* <svg width="296" height="30" viewBox="0 0 296 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect opacity="0.3" x="0.293579" y="0.299316" width="295.413" height="28.8819" fill="#4FB0E8"/>
+</svg> */}
