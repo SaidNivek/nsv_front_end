ad96436de540fe1c831d286531918610b594c544
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-25 22:53:51 -0500

    Begin Brands comp, add SectionHead

--- src/components/Home_Components/Brands.js
+++ src/components/Home_Components/Brands.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { SectionHead } from '../../assets'
 
 function Brands() {
   return (
-    <div>Brands</div>
+    <div className="flex flex-col">
+      <div className="text-center relative">
+          <SectionHead text="Brands We Carry" width="278" height="30" viewBox="0 078 30" rect_x="0.472778" rect_y="0.299316" rect_width="277.054" rect_height="28.8819" />
+      </div>    
+    </div>
   )
 }
 
 export default Brands
+{/* <svg width="278" height="30" viewBox="0 0 278 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect opacity="0.3" x="0.472778" y="0.299316" width="277.054" height="28.8819" fill="#4FB0E8"/>
+</svg> */}
