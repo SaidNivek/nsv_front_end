212f594c3243abb1089e226b5d5d2169fe49b4c6
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-30 19:40:58 -0500

    Copy and images in whyShop comp, general layout

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,8 @@
 import close from './close.svg'
 import logo from './logo.svg'
 import menu from './menu.svg'
+import oldStorePic from './oldStorePic.svg'
+import insideStore from './insideStore.svg'
 import arrow from './arrow.svg'
 import SectionHead from '../components/SectionHead.js'
 import bullseye from './bullseye.png'
@@ -12,4 +14,6 @@ export {
     arrow,
     bullseye,
     SectionHead,
+    oldStorePic,
+    insideStore
 }
--- src/components/Home_Components/WhyShopWithUs.js
+++ src/components/Home_Components/WhyShopWithUs.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import { SectionHead } from '../../assets'
+import { SectionHead, oldStorePic, insideStore } from '../../assets'
+
 
 function WhyShopWithUs() {
   return (
     <div className="flex flex-col">
     <div className="text-center relative">
         <SectionHead text="Why shop with us?" width="296" height="30" viewBox="0 0 296 30" rect_x="0.293579" rect_y="0.299316" rect_width="295.413" rect_height="28.8819" />
+    </div>
+    <div className="flex">
+      <div className="flex flex-col w-[50%] ">
+        <p>Shopping with our local, family-owned vacuum sales and service store is not just a transaction; it's an investment in quality, expertise, and a sense of community.</p>
+        <br /> 
+        <p>Since 1934, we've been committed to offering top-notch sales and service for residential, commercial, and central vacuums. When you choose us, you're not just a customer; you're a valued member of our extended family.</p>
+        <br />
+        <p>Our team of experts knows vacuums inside and out, ensuring that you receive personalized recommendations and unmatched service. By supporting us, you're also supporting your local community and helping to preserve a legacy of trust and reliability that spans generations.</p>
+        <br />
+        <p>Choose local, choose family, and choose quality – choose North Shore Vacuum for all your vacuum needs.</p>
+      </div>
+      <div>
+        <img src={oldStorePic} alt="old vacuum store" />
+        <img src={insideStore} alt="inside new vacuum store" />
+      </div>
     </div>    
   </div>
   )
