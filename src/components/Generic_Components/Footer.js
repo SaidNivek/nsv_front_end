a1041b88aa40247d068fab3ff98ed15a0eb865d7
SaidNivek <kevin.vi.dias@gmail.com>
2024-01-31 21:25:39 -0500

    Add some style to footer

--- src/components/Generic_Components/Footer.js
+++ src/components/Generic_Components/Footer.js
@@ -4,17 +4,19 @@ import { navLinks } from '../../constants'
 
 function Footer() {
   return (
-    <footer>
+    <footer className="flex justify-between">
       <img src={logo} alt="northshorevacuum" className="w-[124px] h-[32px]" />
-      {navLinks?.map((nav, index) => (
-            <a key={nav.id}
-            className={`cursor-pointer text-[16px] text-black 
-              // This dynamic code will change the margin-right to prevent margin from apperaing on the last item in the array
-              ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'}`}          
-            href={`#${nav.id}`}>
-              {nav.title}
-            </a>
-        ))}
+      <div>
+        {navLinks?.map((nav, index) => (
+          <a key={nav.id}
+          className={`cursor-pointer text-[16px] text-black 
+          // This dynamic code will change the margin-right to prevent margin from apperaing on the last item in the array
+          ${index === navLinks.length -1 ? 'mr-0' : 'mr-10'}`}          
+          href={`#${nav.id}`}>
+                {nav.title}
+              </a>
+          ))}
+        </div>
     </footer>
   )
 }
