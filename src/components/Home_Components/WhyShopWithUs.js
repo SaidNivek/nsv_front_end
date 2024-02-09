700cb3fd7bbff01e1bddc46312a08533681e2f4b
SaidNivek <kevin.vi.dias@gmail.com>
2024-02-09 18:05:52 -0500

    Why Shop With Us comp styling, tablet size needs work

--- src/components/Home_Components/WhyShopWithUs.js
+++ src/components/Home_Components/WhyShopWithUs.js
@@ -4,12 +4,12 @@ import { SectionHead, oldStorePic, insideStore } from '../../assets'
 
 function WhyShopWithUs() {
   return (
-    <div className="flex flex-col">
+  <div className="flex flex-col md:mb-[16px] ">
     <div className="text-center relative">
         <SectionHead text="Why shop with us?" width="296" height="30" viewBox="0 0 296 30" rect_x="0.293579" rect_y="0.299316" rect_width="295.413" rect_height="28.8819" />
     </div>
-    <div className="flex">
-      <div className="flex flex-col w-[50%] ">
+    <div className="flex flex-col md:flex-row justify-evenly mt-[16px]">
+      <div className="flex flex-col md:w-[43%] w-[90%] align-center ml-5">
         <p>Shopping with our local, family-owned vacuum sales and service store is not just a transaction; it's an investment in quality, expertise, and a sense of community.</p>
         <br /> 
         <p>Since 1934, we've been committed to offering top-notch sales and service for residential, commercial, and central vacuums. When you choose us, you're not just a customer; you're a valued member of our extended family.</p>
@@ -18,9 +18,15 @@ function WhyShopWithUs() {
         <br />
         <p>Choose local, choose family, and choose quality – choose North Shore Vacuum for all your vacuum needs.</p>
       </div>
-      <div>
-        <img src={oldStorePic} alt="old vacuum store" />
-        <img src={insideStore} alt="inside new vacuum store" />
+      <div className="w-[43%] relative hidden xs:block">
+        <img src={oldStorePic} alt="old vacuum store" className="absolute md:w-[45%]"/>
+        <img src={insideStore} alt="inside new vacuum store" className="absolute top-[10%] left-[40%] md:w-[45%]" />
+      </div>
+      <div className="block xs:hidden">
+        <div className="relative">
+          <img src={oldStorePic} alt="old vacuum store" className="absolute top-5 left-[5%] w-[60%]"/>
+          <img src={insideStore} alt="inside new vacuum store" className="absolute top-16 right-[5%] w-[60%]" />
+        </div>
       </div>
     </div>    
   </div>
