8fa9e88cc30fef895a27fc759ad265ea49795a08
SaidNivek <kevin.vi.dias@gmail.com>
2024-02-13 19:50:16 -0500

    Stub out Products page

--- src/containers/Products.jsx
+++ src/containers/Products.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Products() {
   return (
-    <div>Products</div>
+    <>
+      <h2>Our Products</h2>
+      <p>Product Nav - Residential Central Vacuum Commercial</p>
+      <p>Details and image</p>
+      <p>Residential Heading</p>
+      <p>Residential Copy</p>
+      <p>Brands We Carry</p>
+      <p>Brand Logos</p>
+      <p>Central Vacuum Heading</p>
+      <p>Central Vacuum Copy</p>
+      <p>Brands We Carry</p>
+      <p>Brand Logos</p>
+      <p>Commercial Heading</p>
+      <p>Commercial Copy</p>
+      <p>Brands We Carry</p>
+      <p>Brand Logos</p>
+    </>
   )
 }
 
