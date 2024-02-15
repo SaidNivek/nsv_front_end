cab54a217c7a638f825ca66b7f13b7b5c6460e44
SaidNivek <kevin.vi.dias@gmail.com>
2024-02-14 21:58:13 -0500

    Services stubbed out

--- src/containers/Services.jsx
+++ src/containers/Services.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>Services</div>
+    <div>
+      <h2>Services</h2>
+      <p>Services Nav</p>
+      <br />
+      <p>Repairs header</p>
+      <p>Repairs Copy</p>
+      <p>Services List</p>
+      <br />
+      <p>Central Vacuum Install header</p>
+      <p>Central Vacuum Install Copy</p>
+      <p>Services List</p>
+      <br />
+      <p>Central Vacuum Repair header</p>
+      <p>Central Vacuum Repair Copy</p>
+      <p>Services List</p>
+      <br />
+    </div>
   )
 }
 
