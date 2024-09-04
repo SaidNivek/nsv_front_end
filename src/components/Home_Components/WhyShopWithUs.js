87b4b64eb605ba21a1caf198a5fa0cdfaac0f2cb
SaidNivek <kevin.vi.dias@gmail.com>
2024-09-04 16:27:43 -0400

    Images in WhyShopWithUs css sizing changes

--- src/components/Home_Components/WhyShopWithUs.js
+++ src/components/Home_Components/WhyShopWithUs.js
@@ -4,12 +4,12 @@ import { SectionHead, oldStorePic, insideStore } from '../../assets'
 
 function WhyShopWithUs() {
   return (
-  <div className="flex flex-col md:mb-[16px] ">
+  <div className="flex flex-col md:mb-[16px] mr-[2%]">
     <div className="text-center relative">
         <SectionHead text="Why shop with us?" width="296" height="30" viewBox="0 0 296 30" rect_x="0.293579" rect_y="0.299316" rect_width="295.413" rect_height="28.8819" />
     </div>
     <div className="flex flex-col md:flex-row justify-evenly mt-[16px]">
-      <div className="flex flex-col align-center ml-5">
+      <div className="flex flex-col align-center ml-5 mb-5">
         <p>Shopping with our local, family-owned vacuum sales and service store is not just a transaction; it's an investment in quality, expertise, and a sense of community.</p>
         <br /> 
         <p>Since 1934, we've been committed to offering top-notch sales and service for residential, commercial, and central vacuums. When you choose us, you're not just a customer; you're a valued member of our extended family.</p>
@@ -18,16 +18,16 @@ function WhyShopWithUs() {
         <br />
         <p>Choose local, choose family, and choose quality – choose North Shore Vacuum for all your vacuum needs.</p>
       </div>
-      <div className="w-[90%] relative hidden xs:block">
-        <div className="md:ml-[10px]">          
-          <img src={oldStorePic} alt="old vacuum store" className="absolute md:w-[45%] w-[30%]"/>
-          <img src={insideStore} alt="inside new vacuum store" className="absolute top-[10%] left-[40%] md:w-[45%] w-[30%]" />
+      <div className="w-[90%] relative hidden sm:block">
+        <div className="ml-[8%]">          
+          <img src={oldStorePic} alt="old vacuum store" className="absolute md:w-[45%] w-[30%] left-[35%] "/>
+          <img src={insideStore} alt="inside new vacuum store" className="absolute top-[75px] left-[55%] md:w-[45%] w-[30%]" />
         </div>
       </div>
-      <div className="block xs:hidden">
+      <div className="block sm:hidden mb-[-9%] xs:mb-[-14%]">
         <div className="relative ">
-          <img src={oldStorePic} alt="old vacuum store" className="absolute w-[50%] top-5 left-[5%] w-[60%]"/>
-          <img src={insideStore} alt="inside new vacuum store" className="absolute w-[50%] top-16 right-[5%] w-[60%]" />
+          <img src={oldStorePic} alt="old vacuum store" className="absolute w-[30%] left-[30%] xs:w-[25%]"/>
+          <img src={insideStore} alt="inside new vacuum store" className="absolute w-[30%] mt-[10%] left-[50%] xs:w-[25%] " />
         </div>
       </div>
     </div>    
