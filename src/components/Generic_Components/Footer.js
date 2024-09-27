f7f3f365db5469c4aa81b15cd41352b41ec558ad
SaidNivek <kevin.vi.dias@gmail.com>
2024-09-27 12:17:33 -0400

    Update footer

--- src/components/Generic_Components/Footer.js
+++ src/components/Generic_Components/Footer.js
@@ -6,8 +6,8 @@ import { NavLink } from 'react-router-dom'
 function Footer() {
   return (
     <footer className='mt-[3.75] md:mt-[2.5rem]'>
-        <nav className="flex md:flex-row-reverse flex-col justify-between p-10 md:mt-0 mt-[2.5rem]">
-          <p>Address phone number</p>
+        <nav className="flex md:flex-row flex-col justify-between p-10 md:mt-0 mt-[2.5rem] items-center">
+
                 {/* <div className="list-none sm:flex hidden justify-end item-center flex-1">
             {navLinks.map((nav, index) => (
               <NavLink to={`${nav.id}`} className={`cursor-pointer text-[16px] text-black hover:underline hover:decoration-[#4FB0E8] hover:decoration-4 
@@ -18,6 +18,10 @@ function Footer() {
             ))}
           </div> */}
           <img src={logo2} alt="northshorevacuum" className=" md:mt-0 mt-5 w-[40%] " />
+          <div className="md:text-right mt-[1rem] md:mt-0">
+            <p>240 South Main St, Middleton, MA 01949</p>
+            <p>(978) 774-1155</p>
+          </div>
         </nav>
       </footer>
   )
