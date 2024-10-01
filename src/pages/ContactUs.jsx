9142e09b6385002e299a785e7ee693534907035d
SaidNivek <kevin.vi.dias@gmail.com>
2024-10-01 13:10:09 -0400

    Styling mostly complete, need to look at some spacing/padding

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -62,65 +62,61 @@ function ContactUs() {
   };
 
   return (
-    <div className="max-w-[960px] flex md:flex-col md:m-auto">
+    <div className="md:max-w-[960px] flex flex-col md:m-auto w-[90%] m-auto">
       <h2>Contact Us</h2>
-      <p>For estimates or questions, we’d love to hear from you.</p>
-      <div className='flex md:flex-row flex-col gap-[4rem] justify-between'>
+      <p className="mt-[8px]">For estimates or questions, we’d love to hear from you.</p>
+      <div className='flex md:flex-row flex-col gap-[4rem] justify-between mt-[24px]'>
       {!isFormSubmitted ? (
-        <div className="ContactForm min-w-[320px] w-full p-[24px]  ">
-          <div  >
+        <div className="ContactForm min-w-[320px] flex flex-col w-full p-[24px] gap-[24px] ">
+          <div  className="flex flex-col gap-[8px]">
             <label for="name">
               Name
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
-              placeholder="Name"
               name="name"
               value={name}
               onChange={handleChangeInput}
             />
           </div>
-          <div className="" >
+          <div className="flex flex-col gap-[8px]" >
             <label for="phone">
               Phone
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
               name="phone"
-              placeholder="Phone"
               value={phone}
               onChange={handleChangeInput}
             />
 
           </div>
-          <div className="">
+          <div className="flex flex-col gap-[8px]">
             <label for="email">
               Email
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className=" appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
-              placeholder="Email"
               name="email"
               value={email}
               onChange={handleChangeInput}
             />
           </div>
-          <div>
+          <div className="flex flex-col flex-grow gap-[8px]">
             <label for="message">
               Message
             </label>
             <textarea
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Your message"
+              className="py-2 px-3 appearance-none border rounded w-full leading-tight focus:outline-none focus:shadow-outline h-[100%] min-h-[80px]"
               value={message}
               name="message"
               onChange={handleChangeInput}
             />
           </div>
-          <button type="button" className="w-full text-white border bg-black rounded " onClick={handleSubmit}>
+          <button type="button" className="mt-2 w-full text-white border bg-black rounded h-[3rem]" onClick={handleSubmit}>
             {loading ? "Sending" : "Send"}
           </button>
         </div>
