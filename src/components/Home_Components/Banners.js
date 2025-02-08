94d6cd68d6052b050a0c214e672f4f0b54365537
adam simonelli <adamsimonelli@adams-mbp-2.lan>
2025-02-08 12:38:39 -0500

    Added home banner. Removed center alignment on value cards.

--- src/components/Home_Components/Banners.js
+++ src/components/Home_Components/Banners.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import bannerImage from '../../assets/banner-weveMoved.png';
 
 function Banners() {
   return (
     <div className="homeBanner">
-      <img src='../../assets/banner-weveMoved.png' alt="We've Moved!" />    
+      <img src={bannerImage} alt="Thank you Lynn. Come see us in Middleton" className="" />
     </div>
   )
 }
